d79beabcc03e01f05b2cb7386f9e624cb6641a45
ohmarinseries <omar.hurem@gmail.com>
2022-03-19 14:42:27 +0100

    :lipstick: thank you pimp up

--- src/pages/thankyou.jsx
+++ src/pages/thankyou.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Lottie from 'react-lottie';
+import animationData from '../assets/lf20_vooyqrnt.json'
+import useSearch from 'react-router-dom'
 
 const ThankYou = () => {
     return(
-        <div>
-            <h1>Thank you for submitting this form </h1>
+        <div className="thank-you-gradient vh-100 vw-100">
+            <div className="container w-75 h-100 d-flex flex-column justify-content-center align-content-center ">
+              <div className="container d-flex flex-column justify-content-center align-content-center text-center mb-5 mt-5">
+                <h1>Hvala na prijavi! </h1>
+              </div>
+              <div>
+                  <Lottie options={{animationData:animationData, rendererSettings: {
+                          preserveAspectRatio: "xMidYMid slice"
+                      }}} width={400} height={400} />
+              </div>
+            </div>
         </div>
     );
 
